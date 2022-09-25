61e5c6ece1bf19f6a5c3fd087c72552387ed452c
Zelak <zelak312@gmail.com>
2022-09-25 01:25:24 +0000

    Adding regex and getting dirs

---
+++ language.md
@@ -0,0 +1,19 @@
+# Language features - Road Map
+- [ ] Variables
+- [ ] Interpration blocks
+- [ ] Math
+- [ ] Dates
+
+# Variables
+Regex variable can be used (capture groups including named ones)
+```
+$0 - all
+$1 - first capture group
+$test - named capture group called test
+```
+
+# Interpretation blocks
+Interpration blocks are the fondation when it comes to advanced use
+```
+$()
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::fs;
+
+use clap::Parser;
+use regex::Regex;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    #[clap(value_parser)]
+    input: String,
+
+    #[clap(value_parser)]
+    output: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let regex = Regex::new(&cli.input).expect("Invalid regex");
+    let path = "./";
+    let paths = fs::read_dir(path).unwrap();
+
+    for path in paths {
+        if path.is_err() {
+            panic!("{}", path.err().unwrap());
+        }
+
+        let curr_path = path.unwrap();
+        let file_name = curr_path
+            .file_name()
+            .to_str()
+            .expect("Couldn't get file_name")
+            .to_owned();
+
+        if let Some(captues) = regex.captures(&file_name) {
+            println!("{:?}", captues);
+        }
+    }
 }
